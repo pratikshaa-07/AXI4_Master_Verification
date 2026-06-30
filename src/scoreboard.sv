986f48f7f27b5e00eeaf096e24e526e25d3c645b
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-06-30 16:29:18 +0530

    Update scoreboard.sv

--- src/scoreboard.sv
+++ src/scoreboard.sv
@@ -1,1 +1,8 @@
+class scoreboard extends uvm_scoreboard;
+  `uvm_component_utils(scoreboard)
 
+  function new(string name="",uvm_componet parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
