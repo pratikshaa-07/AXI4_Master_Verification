284a71b8d89278ec1624490d53822268d347d68f
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-06-30 16:54:03 +0530

    Update sequence_item.sv

--- src/sequence_item.sv
+++ src/sequence_item.sv
@@ -1,6 +1,5 @@
 class seq_item extends uvm_sequence_item;
   
-  //write phase signals
   rand  bit [7:0]   sop;
   randc bit [3:0]   txn_id;
   rand bit  [31:0]  waddr;
@@ -10,17 +9,9 @@ class seq_item extends uvm_sequence_item;
   rand bit  [1:0]   lock;
   rand bit  [1:0]   cache;
   rand bit  [2:0]   prot;
-  rand bit  [127:0] strobe;
+  rand bit          strobe[];
   rand bit  [7:0]   eop;
-  
-  rand bit  [7:0]   wrdata[];
-  bit  wr_en;
-  bit full;
-  
-  //read phase signals
-  bit [7:0] rd_data[];
-  bit rd_en;
-  bit empty;
+  rand bit  [7:0]   data[];
   
   function new(string name="");
     super.new(name="");
@@ -29,7 +20,7 @@ class seq_item extends uvm_sequence_item;
   `uvm_object_utils_begin(seq_item)
   `uvm_field_int(sop,UVM_ALL_ON)
   `uvm_field_int(txn_id,UVM_ALL_ON)
-  `uvm_field_int(waddr,UVM_ALL_ON)
+  `uvm_field_int(addr,UVM_ALL_ON)
   `uvm_field_int(len,UVM_ALL_ON)
   `uvm_field_int(size,UVM_ALL_ON)
   `uvm_field_int(burst,UVM_ALL_ON)
@@ -38,20 +29,25 @@ class seq_item extends uvm_sequence_item;
   `uvm_field_int(prot,UVM_ALL_ON)
   `uvm_field_int(strobe,UVM_ALL_ON)
   `uvm_field_int(eop,UVM_ALL_ON)
-  `uvm_field_int(wrdata,UVM_ALL_ON)
-  `uvm_field_int(wr_en,UVM_ALL_ON)
-  `uvm_field_int(full,UVM_ALL_ON)
-  
-  `uvm_field_int(rd_data,UVM_ALL_ON)
-  `uvm_field_int(rd_en,UVM_ALL_ON)
-  `uvm_field_int(empty,UVM_ALL_ON)
-  
+  `uvm_field_int(rdata,UVM_ALL_ON)
+  `uvm_field_int(
   `uvm_object_utils_end
 
   //basic constraints 
 
   constraint burst_size {
-    size inside {};
+    burst inside {0,1,2};
   }
+
+  constraint sop_val {
+    sop==8'b101010110;
+  }
+
+  constraint eop_val {
+    eop==8'b10101001;
+  }
+
+  constraint 
+  
   
 endclass
