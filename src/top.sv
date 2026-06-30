29936863115d5b96a556e740036e965b605acaf1
amruta828 <amrutanmayi6@gmail.com>
2026-06-30 14:18:15 +0530

    Add top module with clock and reset logic

--- src/top.sv
+++ src/top.sv
@@ -1,1 +1,29 @@
+`include "uvm_macros.svh"
+`include "interface.sv"
 
+module top();
+ import uvm_pkg::*;
+
+ bit clk=0;
+ 
+ always#5 clk=~clk;
+  
+  initial begin
+    rst = 0;
+    #20;
+    rst = 1;
+end
+
+  interface inf(clk,rst);
+
+initial begin
+  uvm_config_db#(virtual interface)::set(uvm_root::get(),"*","vif",vif);
+  
+end
+
+  initial begin
+    run_test("test");
+    #1000 $finish;
+  end
+  
+endmodule
