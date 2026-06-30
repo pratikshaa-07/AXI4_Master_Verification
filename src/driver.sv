2822926f385bb597673c8ac6e481dfa62755f3e3
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-06-30 12:27:01 +0530

    Update driver.sv

--- src/driver.sv
+++ src/driver.sv
@@ -1,1 +1,33 @@
+class driver extends uvm_driver#(seq_item);
+  `uvm_component_utils(driver);
 
+  //virtual interface
+  virtual inf.DRV vif;
+  
+  // to track the sequence count
+  int i;
+  function new(string name="",uvm_component parent);
+    super.new(name,parent);
+    i=0;
+  endfunction
+
+  function void build_phase(uvm_phase);
+    super.build_phase(phase);
+    if(!(uvm_config_db#(virtual inf)::get(this,"","vif",vif)))
+      `uvm_fatal("DRIVER","Virtual inteface not set");
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    repeat(3)@(vif.drv_cb);
+    forever
+      begin
+        seq_item_port.get_next_item(req);
+        drive();
+        seq_item_port.item_done();
+      end
+  endtask
+
+  task drive();
+  endtask
+endclass
