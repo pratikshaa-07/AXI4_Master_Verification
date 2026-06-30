b98213d27049346193e648a5baa6ae701c682782
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-06-30 14:28:10 +0530

    Update monitor.sv

--- src/monitor.sv
+++ src/monitor.sv
@@ -1,1 +1,25 @@
+class monitor extends uvm_monitor;
+  `uvm_component_utils(monitor)
+  //virtual inf
+  virtual inf.MON vif;
+  //to 
+  int i;
+  function new(string name="",uvm_component parent);
+    super.new(name,parent);
+    i=0;
+  endfunction
 
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    repeat(4)@(vif.mon_cb);
+    forever
+      begin
+        mon();
+      end
+  endtask
+
+  task mon();
+    
+  endtask
+  
+endclass
