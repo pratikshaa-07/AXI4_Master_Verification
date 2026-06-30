c0a8235fad80e0f9ef0b2767df9441551b80da4f
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-06-30 12:58:13 +0530

    Update sequence_item.sv

--- src/sequence_item.sv
+++ src/sequence_item.sv
@@ -1,1 +1,19 @@
+class seq_item extends uvm_sequence_item;
+  
+  //write data
+  bit       [7:0]   sop;
+  randc bit [3:0]   txn_id;
+  rand bit  [31:0]  addr;
+  rand bit  [3:0]   len;
+  rand bit  [2:0]   size;
+  rand bit  [1:0]   burst;
+  rand bit  [1:0]   lock;
+  rand bit  [1:0]   cache;
+  rand bit  [2:0]   prot;
+  rand bit  [3:0]   strobe;
+  bit       [7:0]   eop;
 
+  function new(string 
+  `uvm_object_utils_begin(seq_item)
+  
+endclass
