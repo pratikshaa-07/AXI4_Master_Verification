4f0e9811df731ee814fab28b1d6c9ac82e44ebdd
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-07-20 13:06:03 +0530

    Refactor drive task in driver class
    Refactor drive task to use total_bits and remove old logic.

--- src/driver.sv
+++ src/driver.sv
@@ -34,7 +34,10 @@
 
 class driver extends uvm_driver #(seq_item);
   `uvm_component_utils(driver)
-
+  bit[127:0] fifo_data;
+  int total_bits;
+  bit packed_bits;
+  
   virtual inf.DRV vif;
   int i;
 
@@ -61,62 +64,14 @@ class driver extends uvm_driver #(seq_item);
     end
   endtask
 
+    task drive();
+      total_bis=req.pack(packed_bits);
 
-  task drive();
-
-    bit [127:0] fifo_word;
-    byte unsigned pkt_q[$];
-    int idx;
-
-    pkt_q.push_back(req.sop);
-    pkt_q.push_back({4'b0,req.txn_id});
-
-    pkt_q.push_back(req.addr[31:24]);
-    pkt_q.push_back(req.addr[23:16]);
-    pkt_q.push_back(req.addr[15:8]);
-    pkt_q.push_back(req.addr[7:0]);
-
-    pkt_q.push_back({4'b0,req.len});
-    pkt_q.push_back({5'b0,req.size});
-    pkt_q.push_back({6'b0,req.burst});
-    pkt_q.push_back({6'b0,req.lock});
-    pkt_q.push_back({6'b0,req.cache});
-    pkt_q.push_back({5'b0,req.prot});
-
-    foreach(req.strobe[k])
-      pkt_q.push_back({7'b0,req.strobe[k]});
-
-    foreach(req.data[k])
-      pkt_q.push_back(req.data[k]);
-
-    pkt_q.push_back(req.eop);
-
-    idx = 0;
-
-    while(idx < pkt_q.size()) begin
-
-      fifo_word = '0;
-
-      for(int j=0;j<16;j++) begin
-        if(idx < pkt_q.size()) begin
-          fifo_word[(15-j)*8 +: 8] = pkt_q[idx];
-          idx++;
+      //driving
+      for(int j=0;j<128;j++)
+        begin
+          
         end
-      end
-
-      @(vif.drv_cb);
-      wait(vif.drv_cb.full == 0);
-      
-      vif.drv_cb.wr_en   <= 1'b1;
-      vif.drv_cb.wr_data <= fifo_word;
-
-      @(vif.drv_cb);
-
-      vif.drv_cb.wr_en <= 1'b0;
-      vif.drv_cb.wr_data <= '0;
-
-    end
-
-  endtask
+    endtask
 
 endclass
