cc859ac17fbecc7c55e22e8e29bbc4cec9c56db2
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-07-24 11:58:10 +0530

    Update active_monitor.sv

--- src/active_monitor.sv
+++ src/active_monitor.sv
@@ -1,48 +1,22 @@
-// class active_monitor extends uvm_monitor;
-//   `uvm_component_utils(active_monitor)
-//   //virtual inf
-//   virtual inf.MON vif;
-//   //to 
-//   int i;
-//   function new(string name="",uvm_component parent);
-//     super.new(name,parent);
-//     i=0;
-//   endfunction
-
-//   task run_phase(uvm_phase phase);
-//     super.run_phase(phase);
-//     repeat(4)@(vif.mon_cb);
-//     forever
-//       begin
-//         mon();
-//       end
-//   endtask
-
-//   task mon();
-    
-//   endtask
-  
-// endclass
-
-class monitor extends uvm_monitor;
-  `uvm_component_utils(monitor)
-
+class act_monitor extends uvm_monitor;
+  `uvm_component_utils(act_monitor)
+ 
   virtual inf.MON vif;
-  uvm_analysis_port #(seq_item) send_port;
-
+  uvm_analysis_port #(seq_item) act_port;
+ 
   seq_item tr;
-
+ 
   function new(string name = "", uvm_component parent);
     super.new(name, parent);
-    send_port = new("send_port", this);
+    send_port = new("act_port", this);
   endfunction
-
+ 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     if (!uvm_config_db#(virtual inf.MON)::get(this, "", "vif", vif))
-      `uvm_fatal("MON", "Unable to access Interface");
+      `uvm_fatal("ACT_MON", "Unable to access Interface");
   endfunction
-
+ 
   task run_phase(uvm_phase phase);
     repeat(4) @(vif.mon_cb);
     forever begin
@@ -50,10 +24,9 @@ class monitor extends uvm_monitor;
       tr.wr_en   = vif.mon_cb.wr_en;
       tr.wr_data = vif.mon_cb.wr_data;
       tr.rd_en   = vif.mon_cb.rd_en;
-      tr.rd_data = vif.mon_cb.rd_data;
-      send.write(tr);
+      act_port.write(tr);
       @(vif.mon_cb);
     end
   endtask
-
+ 
 endclass
